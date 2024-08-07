029d9e8b098761768d4a6a6f7bb26fa60bf45aa5
efimovvlad <efimovvlad72@gmail.com>
2024-08-08 02:09:55 +0300

    delete merge symbols

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,17 +9,10 @@ app_name = 'api'
 
 router_V1 = DefaultRouter()
 router_V1.register('users', UserViewSet, basename='users')
-=======
-
-from .views import TitleViewSet, CategoryViewSet, GenreViewSet
-
-router_V1 = DefaultRouter()
->>>>>>> 89077111e7bbcecb9157b698e59a6c6027fa57df
 router_V1.register('titles', TitleViewSet)
 router_V1.register('categories', CategoryViewSet, basename='categories')
 router_V1.register('genres', GenreViewSet, basename='genres')
 
-<<<<<<< HEAD
 urlpatterns = [
     path('v1/auth/signup/', SignupAPIView.as_view()),
     path('v1/auth/token/', TokenAPIView.as_view()),
@@ -30,9 +23,4 @@ urlpatterns = [
         'patch': 'partial_update',
         'delete': 'destroy'
     })),
-=======
-
-urlpatterns = [
-    path('v1/', include(router_V1.urls)),
->>>>>>> 89077111e7bbcecb9157b698e59a6c6027fa57df
 ]
