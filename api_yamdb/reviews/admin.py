c38a38d1cea0924981f13f42baf250d83c708e6b
solomongrad <solomongrad@gmail.com>
2024-08-11 00:35:13 +0300

    ebanaya adminka

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -4,19 +4,6 @@ from django.db.models import Avg
 from .models import Title, Category, Genre
 
 class TitleAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'name',
-        'year',
-        'description',
-        'category',
-        'get_genre',
-        'count_reviews',
-        'get_rating'
-    )
-    empty_value_display = 'значение отсутствует'
-    list_filter = ('name',)
-    search_fields = ('name', 'year', 'category')
 
     def get_genre(self, object):
         """Получает жанр или список жанров произведения."""
@@ -33,9 +20,26 @@ class TitleAdmin(admin.ModelAdmin):
     def get_rating(self, object):
         """Вычисляет рейтинг произведения."""
         rating = object.reviews.aggregate(average_score=Avg('score'))
-        return round(rating.get('average_score'), 1)
+        if rating:
+            return round(rating.get('average_score'))
+        return None
 
     get_rating.short_description = 'Рейтинг'
+
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'description',
+        'category',
+        'get_genre',
+        'count_reviews',
+        'get_rating'
+    )
+    empty_value_display = 'значение отсутствует'
+    list_filter = ('name',)
+    search_fields = ('name', 'year', 'category')
+
 
 admin.site.register(Category)
 admin.site.register(Genre)
